Fixed eci_to_lla using slightly wrong formula; fixed NWLS, arrays shallow copy; DEEPCOPY to copy arrays!!!!!!!!

orbit_tools/coordinate_transformation.py
@@ -4,6 +4,7 @@
 
 # Modules
 import numpy as np
+import math
 from . import direction_cosine_matrix as dcm
 from . import planet_data as p_d
 from . import time_trans as t_t
@@ -59,7 +60,7 @@ def lla_to_eci(lat, long, alt, date_time, geodetic=True):
     theta_lst = np.deg2rad(theta_lst)
     if geodetic:
         num = (1-f)**2
-        den = np.sqrt(1-(2*f-(f**2)*np.sin(lat)**2))
+        den = np.sqrt(1-(2*f-(f**2))*np.sin(lat)**2)
         r = (re/den) + alt
         r_z = ((re*num)/den) + alt
         z = r_z*np.sin(lat)
@@ -70,27 +71,22 @@ def lla_to_eci(lat, long, alt, date_time, geodetic=True):
     y = r*np.cos(lat)*np.sin(theta_lst)
     return np.array([x, y, z])
 
-# Make angle between 0 and 360
-def deg_mod(angle):
-    if angle < 0:
-        angle %= -360
-    else:
-        angle %= 360
-    return angle
-
 # Right Asecention and Declination to Azimuth and Elevation
 def radec_to_azel(ra, dec, lat, long, date_time):
     dec = np.deg2rad(dec)
     lat = np.deg2rad(lat)
-    long = np.deg2rad(long)
     _, theta_lst = t_t.lst(date_time, long)
-    lha = np.deg2rad(deg_mod(theta_lst-ra))
+    lha = np.deg2rad(math.fmod(theta_lst-ra, 360))
     elevation = np.arcsin(np.sin(lat)*np.sin(dec)
         +np.cos(lat)*np.cos(dec)*np.cos(lha))
     azimuth = np.arctan2(-np.sin(lha)*np.cos(dec)/np.cos(elevation),
         (np.sin(dec)-np.sin(elevation)*np.sin(lat))/(np.cos(elevation)
         *np.cos(lat)))
-    return np.rad2deg(azimuth), np.rad2deg(elevation)
+    elevation = math.fmod(np.rad2deg(elevation), 360)
+    azimuth = math.fmod(np.rad2deg(azimuth), 360)
+    if azimuth < 0:
+        azimuth += 360
+    return azimuth, elevation
 
 def azel_to_radec(az, el, lat, long, date_time):
     az = np.deg2rad(az)
@@ -100,7 +96,7 @@ def azel_to_radec(az, el, lat, long, date_time):
     dec = np.arcsin(np.sin(el)*np.sin(lat)+np.cos(el)*np.cos(lat)*np.cos(az))
     lha = np.rad2deg(np.arctan2(-np.sin(az)*np.cos(el)/np.cos(dec),
     (np.sin(el)-np.sin(dec)*np.sin(lat))/(np.cos(dec)*np.cos(lat))))
-    ra = deg_mod(theta_lst-lha)
+    ra = math.fmod(theta_lst-lha, 360)
     return ra, np.rad2deg(dec)
 
 def state_to_razel(r, date_time, lat, long, alt):

orbit_tools/orbit_determination.py
@@ -3,12 +3,14 @@
 # 2021-06-27
 
 # Module
-from datetime import time
-import sys
+import copy
 import io
+import math
 import numpy as np
 import pandas as pd
 from scipy.integrate import solve_ivp
+import sys
+
 from . import coordinate_transformation as c_t
 from . import equation_of_motion as eom
 from . import planet_data as p_d
@@ -374,14 +376,18 @@ def find_nominal(lat, long, alt, df_raw, fun, mu):
             args=(mu,), rtol=1e-8, atol=1e-8)
         r = np.array([val[-1] for val in sol.y[0:3]])
         v = np.array([val[-1] for val in sol.y[3:6]])
-        print(r, v)
         return r, v
     df_grouped = pd.DataFrame()
-    for x in range(3):
-        df_grouped[f"r{x+1}"] = df_obs["r"][x::3].values
-        df_grouped[f"jd{x+1}"] = df_obs["juliandate"][x::3].values
-    df_grouped["time_diff"] = df_obs["time_diff"][1::3].values
-    print(df_grouped["r1"])
+    obs_to_use = math.floor(len(df_obs["r"])/3)
+    temp_r = df_obs["r"][0:obs_to_use*3]
+    temp_jd = df_obs["juliandate"][0:obs_to_use*3]
+    temp_time_diff = df_obs["time_diff"][0:obs_to_use*3+1]
+    for x in range(obs_to_use):
+        df_grouped[f"r{x+1}"] = temp_r[x::3].values
+        df_grouped[f"jd{x+1}"] = temp_jd[x::3].values
+    df_grouped["time_diff"] = temp_time_diff[1::3].values
+    if len(df_grouped["time_diff"]) > obs_to_use:
+        df_grouped["time_diff"] = df_grouped["time_diff"].drop(-1)
     df_grouped["r"], df_grouped["v"] = df_grouped.apply(lambda row:
         send_raw_observation_to_start(row["r1"], row["r2"], row["r3"],
         row["jd1"], row["jd2"], row["jd3"], row["time_diff"], mu), axis=1).str
@@ -392,9 +398,8 @@ def find_nominal(lat, long, alt, df_raw, fun, mu):
 # Non-linear Weighted Least Squares
 def nlwls(lat, long, alt, df_raw,
     range_err, az_err, el_err, fun, mu):
-    r_nominal, v_nominal, df_obs = \
-        find_nominal(lat, long, alt, df_raw, fun, mu)
-    print(df_obs["r"])
+    r_nominal, v_nominal, df_obs = find_nominal(
+        lat, long, alt, df_raw, fun, mu)
     # While loop setup
     diff = 1000
     tol = 1e-3
@@ -414,7 +419,6 @@ def nlwls(lat, long, alt, df_raw,
     n_type_obs = len(df_true_obs.iloc[0,:])
     df_for_loop = pd.concat([df_raw["datetime"], df_obs["time_diff"],
             df_true_obs], axis=1)
-    print(x_nominal)
     while diff > tol:
         htwh = np.zeros((len(x_nominal), len(x_nominal)))
         htwy = np.zeros(len(x_nominal))
@@ -433,7 +437,7 @@ def nlwls(lat, long, alt, df_raw,
             y = np.array(list(row[2:])) - x_nom_obs
             h = np.zeros((len(x_nominal), len(x_nom_obs)))
             for x in range(len(x_nominal)):
-                x_mod = x_nominal
+                x_mod = copy.deepcopy(x_nominal)
                 x_mod[x] = x_nominal[x]*(1.001)
                 if row["time_diff"] == 0:
                     r_mod_obs = x_mod[0:3]
@@ -449,14 +453,13 @@ def nlwls(lat, long, alt, df_raw,
             htwy +=  np.dot(h, np.dot(w, y))
         p = np.linalg.pinv(htwh)
         dx_hat = np.dot(p, htwy)
-        rms = np.sqrt(np.dot(y, np.dot(w, y)/(n_type_obs*(n_obs-1))))
+        rms = np.sqrt(np.dot(y, np.dot(w, y))/(n_type_obs*(n_obs)))
         if k > 1:
             diff = np.abs((prev_rms-rms)/prev_rms)
-            print(diff)
         prev_rms = rms
         x_nominal += dx_hat
         k += 1
-        if k == 25:
+        if k == 1000:
             raise Exception
     eig_w, eig_v = np.linalg.eig(p)
     confidence = [np.sqrt(p[x, x])*1000 for x in range(3)]